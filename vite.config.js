12cddb4ac340c9e78f7476216bd23ad4c4abbb0e
Dilshani98 <dilshanisubodha@gmail.com>
2025-06-15 21:09:30 +0530

    correct config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/IsharaPortfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
