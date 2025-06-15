0e290a97910086d1062f11e76798764373b47d10
Dilshani98 <dilshanisubodha@gmail.com>
2025-06-15 21:17:59 +0530

    dfgjh

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/IsharaPortfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
