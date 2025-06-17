e69931b6b107a39296ec0dff31bde0b6783df320
dilshani98 <isharasakalasuriya71@gmail.com>
2025-06-17 14:22:02 +0530

    dssdf

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
             <p className="text-muted-foreground">
               Highly motivated Mechanical Engineer with expertise in industrial automation, preventive maintenance, mechanical troubleshooting,
               and equipment reliability for critical plant systems, seeking to leverage skills to revolutionize the industry and improve working
-              environments..
+              environments.
 
             </p>
 
