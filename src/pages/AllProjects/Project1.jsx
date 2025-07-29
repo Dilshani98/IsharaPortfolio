bc24eb0afad5b34b175a889f2ef83a72fa6bd84d
dilshani98 <isharasakalasuriya71@gmail.com>
2025-07-29 15:27:43 +0530

    qwdqwd

--- src/pages/AllProjects/Project1.jsx
+++ src/pages/AllProjects/Project1.jsx
@@ -41,7 +41,7 @@ export const Project1 = () => {
                                 <div
                                     className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
                                 >
-                                    <div className="w-64 h-64 overflow-hidden">
+                                    <div className="w-32 h-32 overflow-hidden">
                                         <img
                                             src={"/projects/Plant.png"}
                                             alt={"1 Ton Reactor Plant"}
