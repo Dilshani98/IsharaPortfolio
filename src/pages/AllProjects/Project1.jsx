f9097e9a5a64d89a35ad349ff53def045c1d9171
dilshani98 <isharasakalasuriya71@gmail.com>
2025-07-29 15:42:25 +0530

    rdhfc

--- src/pages/AllProjects/Project1.jsx
+++ src/pages/AllProjects/Project1.jsx
@@ -54,7 +54,7 @@ export const Project1 = () => {
                     key={index}
                     className="group bg-card rounded-lg overflow-hidden shadow-sm"
                   >
-                    <div className="w-80 h-80 overflow-hidden">
+                    <div className="w-160 h-160 overflow-hidden">
                       <img
                         src={src}
                         alt={`Gallery ${index + 1}`}
