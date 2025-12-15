dbc585c52f8565623577257fafcc0d2e7c8a83b7
dilshani98 <isharasakalasuriya71@gmail.com>
2025-12-15 11:16:31 +0530

    erfere

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Mechanical Engineer - Mechanical Engineer – FMCG Manufacturing & Design Engineer!
+              Mechanical Engineer | FMCG Manufacturing | Design nnovator!
             </h3>
 
             <p className="text-muted-foreground">
